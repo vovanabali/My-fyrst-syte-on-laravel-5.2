775c249084a015c3b05be859a43fd6c4560152bd
Filin Viacheslav <slavanabali@gmail.com>
2017-07-17 23:42:39 +0300

    15 - response

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -6,15 +6,24 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 
 class AboutController extends Controller
 {
     //
     public function show(){
         if (view()->exists('Default.about')){
-            $viwe =  view('Default.about',['title'=>'Новове значение'])->render();
+           /* $viwe =  view('Default.about',['title'=>'Новове значение'])->render();
             echo $viwe;
-            return;
+            return;*/
+
+           //return viwe('Default.about')->withTitle('Новове значение');
+
+            /*$view = view('Default.about')->withTitle('Новове значение')->render();
+            return (new Response($view))->header('Content-Type','newType');*/
+            //return response()->view('Default.about',['title'=>'New Title']);
+
+            //return response()->download('..\gulpfile.js');
         }
         abort(404);
     }
